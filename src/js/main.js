e0bc1d7afd7580362c8a0e2abca8561442094cad
basile-kamm <another-black-kitty@proton.me>
2025-01-14 14:40:58 +0100

    var and files layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerIcon = document.querySelector(".burger-icon");
   const burgerCloseIcon = document.querySelector(".burger-close-icon");
   const burgerContainer = document.querySelector(".burger-container");
+  const body = document.querySelector("body");
 
   burgerIcon.addEventListener("click", function () {
     burgerContainer.classList.remove("hidden");
+    body.classList.add("no-scroll");
   });
   burgerCloseIcon.addEventListener("click", function () {
+    body.classList.remove("no-scroll");
     burgerContainer.classList.add("hidden");
   });
 });
