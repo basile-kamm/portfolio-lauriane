189b296e3f72789ee7de114f1ff81e9ff7c1c3fa
basile-kamm <another-black-kitty@proton.me>
2025-02-07 13:49:19 +0100

    Integration of medias for desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
+const translations = {
+  en: {
+    bannerSubtitle: "Student",
+  },
+  fr: {
+    bannerSubtitle: "Élève",
+  },
+};
+
+const languageSelectors = document.querySelectorAll('input[name="language"]');
+
+languageSelectors.forEach((lgSelect) => {
+  lgSelect.addEventListener("change", (event) => {
+    setLanguage(event.value);
+    console.log("ye");
+  });
+});
+
+const setLanguage = (language) => {
+  if (language == "fr") {
+    console.log(language);
+  } else if (language == "en") {
+    console.log(language);
+  }
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   const burgerIcon = document.querySelector(".burger-icon");
   const burgerCloseIcon = document.querySelector(".burger-close-icon");
@@ -12,6 +38,4 @@ document.addEventListener("DOMContentLoaded", function () {
     body.classList.remove("no-scroll");
     burgerContainer.classList.add("hidden");
   });
-
-  // Change language fonction
 });
